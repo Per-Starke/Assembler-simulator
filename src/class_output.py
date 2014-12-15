3809b6b0ed36b297317fa35d7a0e370dc98d66cd
Per Starke <per@starke-team.de>
2014-12-15 20:55:08 +0100

    working on getting energy on or of (binary: 0 v 1) for output / Port

--- src/class_output.py
+++ src/class_output.py
@@ -1,6 +1,12 @@
 __author__ = 'Per'
 
 class Output(object):
-    def __init__(self, number_in_port)
+    def __init__(self, number_in_port):
         self.Number_in_port = number_in_port
-    
+
+    binaryCode = "00000001"
+    stromAn = None
+    def get_strom_an_oder_aus(self):
+        number_of_output = self.Number+1
+        binary_number_for_output =  binary[- (number_of_output)]
+        
