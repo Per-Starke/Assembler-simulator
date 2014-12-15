4f2c12ede6a2f269840cb66ac3db4f482eb1cf90
Per Starke <per@starke-team.de>
2014-12-15 21:12:35 +0100

    Binary number of output

--- src/class_output.py
+++ src/class_output.py
@@ -5,8 +5,19 @@ class Output(object):
         self.Number_in_port = number_in_port
 
     binaryCode = "00000001"
-    stromAn = None
     def get_strom_an_oder_aus(self):
-        number_of_output = self.Number+1
-        binary_number_for_output =  binary[- (number_of_output)]
-        
+
+        number_of_output = self.Number_in_port
+        binary_number_for_output =   int(binaryCode[number_of_output])
+        if binary_number_for_output == 1:
+            stromAn = True
+        elif binary_number_for_output == 0:
+            stromAn = False
+        else:
+            stromAn = None
+            print("invalid binary number")
+        return stromAn
+
+output1 = Output(7)
+print(output1.get_strom_an_oder_aus())
+
