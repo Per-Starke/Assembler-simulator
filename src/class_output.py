7ea32fcace080f87d010a894d4bf752bcd6fd435
Per Starke <per@starke-team.de>
2014-12-16 18:45:06 +0100

    news on /feature get binary code

--- src/class_output.py
+++ src/class_output.py
@@ -1,14 +1,22 @@
 __author__ = 'Per'
 
+def get_binary_code():
+        binary_code = "00000001"
+        if len(binary_code) == 8:
+            return binary_code
+
+
+
 class Output(object):
     def __init__(self, number_in_port):
         self.Number_in_port = number_in_port
 
-    binaryCode = "00000001"
-    def get_strom_an_oder_aus(self):
 
+
+    def get_strom_an_oder_aus(self):
+        binary_code = get_binary_code()
         number_of_output = self.Number_in_port
-        binary_number_for_output =   int(binaryCode[number_of_output])
+        binary_number_for_output =   int(binary_code[number_of_output])
         if binary_number_for_output == 1:
             stromAn = True
         elif binary_number_for_output == 0:
@@ -16,8 +24,12 @@ class Output(object):
         else:
             stromAn = None
             print("invalid binary number")
+
+        print ("Energy at output" , self.Number_in_port , ":" , stromAn)
         return stromAn
 
-output1 = Output(7)
-print(output1.get_strom_an_oder_aus())
 
+
+
+output1 = Output(7)
+output1.get_strom_an_oder_aus()
