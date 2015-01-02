6205e504f81a11ccf51fdbc8cc0a1f4a05712c73
Per Starke <per@starke-team.de>
2015-01-02 18:08:47 +0100

    working on function to turn every binary number into 8-bit

--- src/translate_assembler.py
+++ src/translate_assembler.py
@@ -2,7 +2,22 @@ __author__ = 'Per'
 
 from binary_class import Binary
 
+def binary_number_into_eight_digits(binary_number):
+    new_binary_number = []
+    while len(binary_number) < 8:
+        new_binary_number.append("0")
+        for number in binary_number:
+            new_binary_number.append(number)
+    return (new_binary_number)
+
 binary_number_class = Binary(2, 24)
 binary_number = binary_number_class.ausrechnen()
 print(binary_number)
+
+
+new_bina = binary_number_into_eight_digits(binary_number)
+print(new_bina)
+
+
+
 registers = ["7", "24", "42"] # Assembler sourcecode in numbers
