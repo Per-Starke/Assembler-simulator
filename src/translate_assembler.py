e9d7da6515010ee25685a0240e8755aeb0bf1263
Per Starke <per@starke-team.de>
2015-01-20 09:54:06 +0100

    renamed functions to turn every binary number into eight digits -> ...- bit trying how to add text files

--- src/translate_assembler.py
+++ src/translate_assembler.py
@@ -2,7 +2,7 @@ __author__ = 'Per'
 
 from binary_class import Binary
 
-def binary_number_into_eight_digits(binary_number):
+def binary_number_into_eight_bit(binary_number):
     new_binary_number = []
     counter_binary = list(binary_number)
     while len(counter_binary) < 8:
@@ -15,7 +15,7 @@ binary_number_class = Binary(2, 1)
 binary_number = binary_number_class.ausrechnen()
 print(binary_number)
 
-new_bina = binary_number_into_eight_digits(binary_number)
+new_bina = binary_number_into_eight_bit(binary_number)
 print(new_bina)
 
 
