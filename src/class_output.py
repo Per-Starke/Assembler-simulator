1e88fd8ebb846fcfc88056bb980c42635dafeb77
Per Starke <per@starke-team.de>
2015-04-01 18:43:31 +0200

    changed capital/non-capital writing in comment, added few empty lines

--- src/class_output.py
+++ src/class_output.py
@@ -2,9 +2,11 @@ __author__ = 'Per'
 
 
 def get_binary_code():             # that function will change again when scanning the assembler works
-        binary_code = "00000001"        # it returns a binary number AS A STRING!
+        binary_code = "00000001"        # it returns a binary number as a string
         if len(binary_code) == 8:        # binary number's lenghts must be 8! I'm working with 8-bit
             return binary_code
+
+
 
 
 class Output(object):                               # one output in the port's
